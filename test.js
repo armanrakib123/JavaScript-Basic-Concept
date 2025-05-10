cd5564f86ac7afd543d12c0541d6da166eebcad0
armanrakib123 <armanrakib61@gmail.com>
2025-05-11 01:04:42 +0600

    TimeSet

---
+++ test.js
@@ -0,0 +1,12 @@
+function getTimeString(time) {
+  //get Hour and rest seconds
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  return `${hour} hour  ${minute} minute ${remainingSecond} second ago`;
+}
+
+console.log(getTimeString(7865));
+
+
